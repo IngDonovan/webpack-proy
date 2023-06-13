9a62e517182d3e211a7689faa04281837eafb32a
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-13 16:11:34 -0500

    add information

--- src/index.js
+++ src/index.js
@@ -6,26 +6,36 @@ const data =
     name: "Donovan Rojas Mendoza",
     nickname: "@ingdrm",
     tecnologias: 'Frontend Developer, #JavaScript #React #Web3',
-    description: "I'am web developer and like it, ",
+    description: "I am an industrial engineer and mechatronics technologist with a passion for web development. My goal is to become a highly skilled full-stack developer and provide innovative technological solutions.With a solid foundation in HTML, CSS, and JavaScript, I have created frontend projects that showcase my ability to design attractive and functional interfaces. My primary focus currently lies in expanding my knowledge in Node.js and developing backend skills to complement my frontend expertise.My background in industrial engineering and mechatronics has provided me with a strong foundation in problem analysis and creative problem-solving. I utilize these skills to tackle web development challenges with a structured and efficient approach.",
     avatar: "https://avatars.githubusercontent.com/u/119254368?s=400&u=79c99a8bb518a60725783d3f07ec4a3c31a45c21&v=4",
     social: 
     [
+      {
+        name: "youtube",
+        url: "https://www.youtube.com/channel/UCTosbAful3tW1T5K_7f9wvQ",
+        username: "@IngeDrm",
+      },
+      {
+        name: "github",
+        url: "https://github.com/IngDonovan",
+        username: "IngDonovan",
+      },
       {
         name: "twitter",
         url: "https://twitter.com/ingdrm",
         username: "@ingdrm",
       },
       {
-        name: "instagram",
-        url: "https://instagram.com/",
-        username: "",
+        name: "linkedin",
+        url: "https://linkedin.com/in/ingdonovanrm",
+        username: "ingdonovanrm",
       },
     ],
     
     links: 
     [
       {
-        name: "blog",
+        name: "Proyects",
         url: "https://example.com/",
         color: "blue",
         emoji: "📖",
@@ -33,20 +43,12 @@ const data =
       {
         name: "podcast",
         url: "https://example.com/",
-        color: "yellow",
+        color: "slate",
         emoji: "💬",
       },
     ],
     footer: "Made with Love on Colombia",
 };
-
-/* <div class="bg-fuchsia-200 px-4 py-5 w-full flex justify-between">
-    <a class="text-sm font-bold text-fuchsia-600 text-center hover:text-fuchsia-800 cursor-pointer"
-    href="https:///l/mentor101" target="_blank">
-    Mentorías
-    </a>
-    <span>💻</span>
-</div> */
 
 const main = ( ) => {
     let name = document.createTextNode(data?.name);
